c035f34c211bd8ac71b60f6b4977528a929a838c
c0a22132 <107174291+c0a22132@users.noreply.github.com>
2024-07-01 08:32:45 +0900

    テスト動作確認用

--- EC/login/database_config.php
+++ EC/login/database_config.php
@@ -1,5 +1,5 @@
 <?php
-define('DSN', 'mysql:host=localhost;dbname=your_database_name;charset=utf8');
+define('DSN', 'mysql:host=localhost;dbname=ecdatabese;charset=utf8');
 define('DB_USER', 'user');
 define('DB_PASS', 'passwordA1!');
 ?>
