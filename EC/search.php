97fb06e74a5bd62d7d01ade5a1b072678ace9f43
c0a22132 <107174291+c0a22132@users.noreply.github.com>
2024-07-11 09:42:26 +0900

    検索機能改良

--- EC/search.php
+++ EC/search.php
@@ -72,4 +72,6 @@ $conn->close();
     </div>
 
     <!-- Materialize JS -->
-    <
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
